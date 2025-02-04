13f1612cd272426821e942aa0731a08d24a45fbb
Fábio Jesus <fabioj@agap2.pt>
2025-02-04 11:39:57 +0000

    first commit

---
+++ Moongt.RD.Launchpad.Generation.Core/Interfaces/IContractGenerator.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Moongt.RD.Launchpad.Generation.Core.Interfaces;
+
+public interface IContractGenerator<TModel>
+{
+    GenerationResult Generate(TModel model, ITemplate? template);
+    ValidationResult Validate(TModel model);
+
+}
